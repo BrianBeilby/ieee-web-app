1f6434443f937eb225206d68ade4d5041b5ee172
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-03-18 16:22:36 -0700

    About Page ++
    -Added Base plate photos
    -Event Showcase elements
    - Word of the day Element
    -Cleaned up css
    -Misc

---
+++ src/components/Card.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="white" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div" sx={{ color: 'white' }}>
+        in{bull}no{bull}va{bull}tive
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="white">
+        adjective
+      </Typography>
+      <Typography variant="body2" sx={{ color: 'white' }}>
+      (of a person) introducing new ideas; original and creative in thinking.
+        <br />
+        {'"an innovative thinker"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      {/*<Button size="small">Learn More</Button>*/}
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined" sx={{ backgroundColor: 'black' }}>
+        {card}
+      </Card>
+    </Box>
+  );
+}
