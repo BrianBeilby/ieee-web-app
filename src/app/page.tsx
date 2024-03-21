511e7acc80cc1d429771a57dc84418ae36b1ef8e
BrianBeilby <brianbeilby98@gmail.com>
2024-03-21 11:56:23 -0700

    Added carousel cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
 import Image from "next/image";
-import { Carousel } from "flowbite-react";
+import { Carousel, CustomFlowbiteTheme } from "flowbite-react";
 import { Spotlight } from "@/components/Spotlight";
 import { TextGenerateEffect } from "@/components/TextGenerateEffect";
+import { useRef } from "react";
+import { eventsData } from "@/app/events/eventData";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const customTheme: CustomFlowbiteTheme["carousel"] = {
+  root: {
+    leftControl: "hidden",
+    rightControl: "hidden",
+  },
+};
 
 export default function Home() {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
     <div className="container my-12 mx-auto px-4 sm:px-6 lg:px-8">
       <section className="bg-center bg-no-repeat bg-transparent bg-blend-multiply">
@@ -30,47 +49,58 @@ export default function Home() {
         </div>
       </section>
       <div className="mx-auto max-w-screen-md h-56 sm:h-64 lg:h-80 xl:h-80 2xl:h-96 relative">
-        <Carousel slide={false} indicators={true}>
-          <div className="relative w-full h-full">
-            <Image
-              fill
-              style={{ objectFit: "cover" }}
-              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-              alt="..."
-            />
-          </div>
-          <div className="relative w-full h-full">
-            <Image
-              fill
-              style={{ objectFit: "cover" }}
-              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-              alt="..."
-            />
-          </div>
-          <div className="relative w-full h-full">
-            <Image
-              fill
-              style={{ objectFit: "cover" }}
-              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-              alt="..."
-            />
-          </div>
-          <div className="relative w-full h-full">
-            <Image
-              fill
-              style={{ objectFit: "cover" }}
-              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-              alt="..."
-            />
-          </div>
-          <div className="relative w-full h-full">
-            <Image
-              fill
-              style={{ objectFit: "cover" }}
-              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-              alt="..."
-            />
-          </div>
+        <Carousel slide={true} theme={customTheme}>
+          {eventsData.map((project, index) => (
+            <div key={index} className="relative w-full h-full">
+              <motion.div
+                ref={ref}
+                style={{
+                  scale: scaleProgess,
+                  opacity: opacityProgess,
+                }}
+                className="group mb-3 sm:mb-8 last:mb-0"
+              >
+                <section className="bg-gray-100 max-w-full border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[25rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+                  <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+                    <h3 className="text-2xl font-semibold">{project.title}</h3>
+                    <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+                      {project.description}
+                    </p>
+                    <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+                      {project.tags.map((tag, index) => (
+                        <li
+                          className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                          key={index}
+                        >
+                          {tag}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <Image
+                    src={project.imageUrl}
+                    alt="Project I worked on"
+                    quality={95}
+                    width={900}
+                    height={900}
+                    className="absolute hidden sm:block top-8 -right-20 w-[28.25rem] rounded-t-lg shadow-2xl
+                            transition 
+                            group-hover:scale-[1.04]
+                            group-hover:-translate-x-3
+                            group-hover:translate-y-3
+                            group-hover:-rotate-2
+
+                            group-even:group-hover:translate-x-3
+                            group-even:group-hover:translate-y-3
+                            group-even:group-hover:rotate-2
+
+                            group-even:right-[initial] group-even:-left-40"
+                  />
+                </section>
+              </motion.div>
+            </div>
+          ))}
         </Carousel>
       </div>
     </div>
