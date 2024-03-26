9a244fe20fc0b31dd492f862b2473c08647011f3
BrianBeilby <brianbeilby98@gmail.com>
2024-03-26 16:22:57 -0700

    Removed :dark queries on events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,7 +7,13 @@ import SectionDivider from "@/components/section-divider";
 import { eventsData } from "@/app/events/eventData";
 import React from "react";
 import Event from "./event";
+import type { CustomFlowbiteTheme } from "flowbite-react";
 
+const customTheme: CustomFlowbiteTheme["button"] = {
+  outline: {
+    on: "flex w-full justify-center transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit bg-gray-900 text-white",
+  },
+};
 
 export default function Events() {
   const [openModal, setOpenModal] = useState(false);
@@ -26,7 +32,7 @@ export default function Events() {
   return (
     <div className="container mx-auto px-4 py-28">
       <h1 className="text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-white">
-        Upcoming Events 
+        Upcoming Events
       </h1>
       <div className="flex flex-col items-center">
         <div className="w-full max-w-6xl mx-auto">
@@ -40,6 +46,7 @@ export default function Events() {
         </div>
         <div className="mt-6 mb-12">
           <Button
+            theme={customTheme}
             outline
             gradientDuoTone="purpleToBlue"
             size="lg"
@@ -52,8 +59,17 @@ export default function Events() {
       </div>
       <Modal
         theme={{
+          root: {
+            show: {
+              on: "flex bg-gray-900 bg-opacity-80",
+            },
+          },
           content: {
-            inner: "relative rounded-lg bg-white shadow dark:bg-gray-900 flex flex-col max-h-[90vh]",
+            inner:
+              "relative rounded-lg shadow bg-gray-900 flex flex-col max-h-[90vh]",
+          },
+          header: {
+            base: "flex items-start justify-between rounded-t border-b p-5 border-gray-600",
           },
         }}
         dismissible
@@ -62,12 +78,12 @@ export default function Events() {
         size="md"
       >
         <Modal.Header>
-          <div className="text-xl font-semibold text-gray-700 dark:text-white">
+          <div className="text-xl font-semibold text-white">
             How to Subscribe to Our Calendar
           </div>
         </Modal.Header>
         <Modal.Body>
-          <div className="text-gray-500 dark:text-gray-400 space-y-4">
+          <div className="text-gray-400 space-y-4">
             <div>
               <label className="text-base font-medium">
                 Use the link below to subscribe in your preferred calendar
@@ -79,7 +95,8 @@ export default function Events() {
                   value={icsLink}
                   readOnly
                   disabled
-                  className="bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg block w-full p-2.5 pr-12 cursor-default disabled:bg-gray-50" />
+                  className="bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg block w-full p-2.5 pr-12 cursor-default disabled:bg-gray-50"
+                />
                 <div className="absolute inset-y-0 right-0 flex items-center pr-3">
                   <Tooltip
                     content={copySuccess ? "Copied!" : "Copy to clipboard"}
@@ -87,7 +104,9 @@ export default function Events() {
                   >
                     <button
                       onClick={copyLinkToClipboard}
-                      className={`text-gray-800 hover:bg-gray-100 rounded-lg p-2 ${copySuccess ? "bg-gray-200" : ""}`}
+                      className={`text-gray-800 hover:bg-gray-100 rounded-lg p-2 ${
+                        copySuccess ? "bg-gray-200" : ""
+                      }`}
                     >
                       {copySuccess ? (
                         <CheckIcon className="w-4 h-4 text-blue-500" />
@@ -105,7 +124,16 @@ export default function Events() {
                 calendar:
               </label>
               <div className="mt-2">
-                <Button href={icsLink} color="light">
+                <Button
+                  href={icsLink}
+                  color="light"
+                  theme={{
+                    color: {
+                      light:
+                        "border focus:ring-4 border-gray-600 bg-gray-600 text-white focus:ring-gray-700 enabled:hover:border-gray-700 enabled:hover:bg-gray-700",
+                    },
+                  }}
+                >
                   Download
                 </Button>
               </div>
@@ -113,21 +141,24 @@ export default function Events() {
           </div>
         </Modal.Body>
       </Modal>
-
-      <div className="flex flex-col items-center justify-center"> {/* Start of events */}
-        <SectionDivider />     
-        <h2 className="text-3xl capitalize text-center text-white sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-8" > Events & Plans</h2>
+      <div className="flex flex-col items-center justify-center">
+        {" "}
+        {/* Start of events */}
+        <SectionDivider />
+        <h2 className="text-3xl capitalize text-center text-white sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-8">
+          {" "}
+          Events & Plans
+        </h2>
         <div>
-          {eventsData && eventsData.map((event, index) => (
-            <React.Fragment key={index}>
-              <Event {...event} />
-            </React.Fragment>
-          ))}
+          {eventsData &&
+            eventsData.map((event, index) => (
+              <React.Fragment key={index}>
+                <Event {...event} />
+              </React.Fragment>
+            ))}
         </div>
-      </div> {/* End of events */}
-                 
+      </div>{" "}
+      {/* End of events */}
     </div>
-
   );
 }
-
