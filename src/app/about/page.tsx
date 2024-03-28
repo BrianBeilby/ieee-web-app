6e80cbb91b0e7baa04c017cc0afbb198e4f1035e
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-03-27 18:32:23 -0700

    Fixed elements with different res
    -fixed the about page "How to be a member" element with widths 1030 - 1270.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -235,80 +235,93 @@ export default function About() {
                 a positive impact on the future of technology!
               </p>
             </motion.div>
+            
             <motion.div
               initial={{ opacity: 0, y: 100 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.2 }}
             >
-              <div className="min-h-screen flex flex-col p-8 sm:p-16 md:p-24 justify-center ">
-                {" "}
+              <div className="min-h-screen flex flex-col p-8 sm:p-16 md:p-24 justify-center">
                 {/* Event Showcase  */}
                 <div data-theme="teal" className="mx-auto max-w-6xl">
                   <h2 className="sr-only">Featured case study</h2>
                   <section className="font-sans text-white">
-                    <div className="[ lg:flex lg:items-center ] [ fancy-corners fancy-corners--large fancy-corners--top-left fancy-corners--bottom-right ] position: relative; transform-style: preserve-3d;">
-                      <div className="flex-shrink-0 self-stretch sm:flex-basis-40 md:flex-basis-50 xl:flex-basis-60">
-                        <div className="h-full">
-                          <article className="h-full">
-                            <div className="h-full">
-                              <div className="relative">
-                                <Image
-                                  width={600}
-                                  height={350}
-                                  src="/images/wp4918510-desktop-bmw-e30-wallpapers.jpg"
-                                  className="h-full object-cover"
-                                  alt={""}
-                                />
-                                <div className="absolute top-0 left-0 w-12 h-12 border-l-4 border-t-4 border-blue-900 -ml-2 -mt-2 rounded-lg"></div>
-                                <div className="absolute bottom-0 right-0 w-12 h-12 border-r-4 border-b-4 border-blue-900 -mr-2 -mb-2 rounded-lg"></div>
+                    <div className="lg:flex lg:items-center">
+                      <div className="flex-shrink-0 self-stretch lg:w-1/2"> {/* Image */}
+                        <motion.div
+                          initial={{ opacity: 0, y: 100 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ delay: 0.4 }}
+                        >
+                          <div className="h-full">
+                            <article className="h-full">
+                              <div className="h-full">
+                                <div className="relative">
+                                  <Image
+                                    width={600}
+                                    height={350}
+                                    src="/images/wp4918510-desktop-bmw-e30-wallpapers.jpg"
+                                    className="h-full object-cover"
+                                    alt={""}
+                                  />
+                                  <div className="absolute top-0 left-0 w-12 h-12 border-l-4 border-t-4 border-blue-900 -ml-2 -mt-2 rounded-lg"></div>
+                                  <div className="absolute bottom-0 right-0 w-12 h-12 border-r-4 border-b-4 border-blue-900 -mr-2 -mb-2 rounded-lg"></div>
+                                </div>
                               </div>
-                            </div>
-                          </article>
-                        </div>
+                            </article>
+                          </div>
+                        </motion.div>
                       </div>
-                      <div className="p-2 bg-grey">
-                        <div className="leading-relaxed">
-                          <h2 className="leading-tight text-4xl font-bold">
-                            How to be a member
-                          </h2>
-                          <p className="mt-4">
-                            Sign up to IEEE to become an official member. and
-                            dive into the exciting world of Electronic
-                            Engineering. Whether you&apos;re a seasoned
-                            enthusiast or simply curious about the field,
-                            everyone is invited to join us in exploring the
-                            latest advancements in electronic technology.
-                          </p>
-                          <p className="mt-4">
-                            It will cost $16 to be an official student member of
-                            IEEE. You can join by clicking the link&nbsp;
-                            <Link
-                              className="group inline-block px-3 py-1 bg-black text-yellow rounded-full border border-black hover:border-grey-950 hover:scale-110 transition duration-300"
-                              href="https://www.ieee.org/membership/join/index.html?WT.mc_id=hc_join"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              here
-                            </Link>
-                            . &nbsp; Once a member, be sure to visit our events
-                            page for more information on upcoming opportunities.
-                          </p>
-                          <p>
-                            <Link
-                              href="/events"
-                              className="mt-2 group bg-black text-white px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-grey-950 active:scale-105 transition"
-                            >
-                              Explore Events{" "}
-                              <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-                            </Link>
-                          </p>
-                        </div>
+                      <div className="p-2 bg-gray lg:w-1/2"> {/* Text */}
+                        <motion.div
+                          initial={{ opacity: 0, y: 100 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ delay: 0.6 }}
+                        >
+                          <div className="leading-relaxed">
+                            <h2 className="leading-tight text-4xl font-bold">
+                              How to be a member
+                            </h2>
+                            <p className="mt-4">
+                              Sign up to IEEE to become an official member and dive into
+                              the exciting world of Electronic Engineering. Whether you're a
+                              seasoned enthusiast or simply curious about the field,
+                              everyone is invited to join us in exploring the latest
+                              advancements in electronic technology.
+                            </p>
+                            <p className="mt-4">
+                              It will cost $16 to be an official student member of IEEE. You
+                              can join by clicking the link&nbsp;
+                              <Link
+                                className="group inline-block px-3 py-1 bg-black text-yellow rounded-full border border-black hover:border-gray-950 hover:scale-110 transition duration-300"
+                                href="https://www.ieee.org/membership/join/index.html?WT.mc_id=hc_join"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                here
+                              </Link>
+                              . &nbsp; Once a member, be sure to visit our events page for
+                              more information on upcoming opportunities.
+                            </p>
+                            <p>
+                              <Link
+                                href="/events"
+                                className="mt-2 group bg-black text-white px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                              >
+                                Explore Events{" "}
+                                <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                              </Link>
+                            </p>
+                          </div>
+                        </motion.div>
                       </div>
                     </div>
                   </section>
                 </div>
               </div>
             </motion.div>
+
+                      
             <OutlinedCard />
             <SectionDivider />
             <div className="container mx-auto px-6 text-lg md:text-xl text-white mb-10 max-w-[45rem] text-center leading-8 sm:mb-10">
