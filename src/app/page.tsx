9af7ffbde81e418dcc8ae54e6a96ab405de14a23
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-02 20:51:05 -0700

    Image carousel arrows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,12 @@ import CarouselItem from "@/components/CarouselItem";
 
 const customTheme: CustomFlowbiteTheme["carousel"] = {
   root: {
-    leftControl: "hidden",
-    rightControl: "hidden",
+    "base": "relative h-full w-full",
+    "leftControl": "absolute -left-8 top-0 flex h-full items-center justify-center px-4 focus:outline-none ",
+    "rightControl": "absolute -right-11 top-0 flex h-full items-center justify-center px-4 focus:outline-none"
+  },  control: {
+    "base": "inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10",
+    "icon": "h-5 w-5 text-white dark:text-gray-900 sm:h-6 sm:w-6"
   },
 };
 
