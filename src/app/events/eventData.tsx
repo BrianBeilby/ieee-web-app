95f86c14b6e2cebe4bdba57f16d2f4e1548dff00
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-03 19:34:04 -0700

    Added sample photo, Fixed carousel controllers
    -left and right controll wouldnt work on some resolutions
    -changed no photo with sample photo so the site would compile

--- src/app/events/eventData.tsx
+++ src/app/events/eventData.tsx
@@ -4,18 +4,18 @@ export const eventsData = [
     title: "Fundraising and Team Establishment",
     description: "Form a dedicated team to efficiently coordinate fundraising events and brainstorm innovative projects for IEEE PES.",
     tags: ["Fundraising", "Project Coordination", "IEEE PES"],
-    
+    imageUrl: "/images/sample.jpeg",
   },
   {
     title: "FE Exam Guidance",
     description: "Provide comprehensive guidance on navigating the FE Exam, offering essential tools for success. Delve into the importance and advantages of power-elective courses.",
     tags: ["FE Exam","Power-elective Courses","Career Development"],
-    
+    imageUrl: "/images/sample.jpeg",
   },
   {
     title: "Alumni Speakers",
     description: "Invite accomplished alumni and industry professionals to share insights, experiences, and career paths in power-related positions, inspiring current members.",
     tags: ["Alumni", "Industry Professionals", "Networking"],
-    
+    imageUrl: "/images/sample.jpeg",
   },
 ] as const;
