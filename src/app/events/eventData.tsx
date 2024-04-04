c907c7afc61ddfeafb7138aee95c3823c68aebab
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-03 17:57:37 -0700

    Removed place holder images
    Removed place holder images from the events page, (pasquale request)

--- src/app/events/eventData.tsx
+++ src/app/events/eventData.tsx
@@ -4,18 +4,18 @@ export const eventsData = [
     title: "Fundraising and Team Establishment",
     description: "Form a dedicated team to efficiently coordinate fundraising events and brainstorm innovative projects for IEEE PES.",
     tags: ["Fundraising", "Project Coordination", "IEEE PES"],
-    imageUrl: "/images/wp4918510-desktop-bmw-e30-wallpapers.jpg",
+    
   },
   {
     title: "FE Exam Guidance",
     description: "Provide comprehensive guidance on navigating the FE Exam, offering essential tools for success. Delve into the importance and advantages of power-elective courses.",
     tags: ["FE Exam","Power-elective Courses","Career Development"],
-    imageUrl: "/images/wp4918510-desktop-bmw-e30-wallpapers.jpg",
+    
   },
   {
     title: "Alumni Speakers",
     description: "Invite accomplished alumni and industry professionals to share insights, experiences, and career paths in power-related positions, inspiring current members.",
     tags: ["Alumni", "Industry Professionals", "Networking"],
-    imageUrl: "/images/wp4918510-desktop-bmw-e30-wallpapers.jpg",
+    
   },
 ] as const;
